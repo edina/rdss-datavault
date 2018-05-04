5de56e818596c1e0e0733daf3ed0ba8d454d57b2
Steven Carlysle-Davies <steven.carlysle-davies@ed.ac.uk>
2018-05-04 14:19:12 +0100

    Enable S3 storage

--- compose/configure-storage.sh
+++ compose/configure-storage.sh
@@ -5,8 +5,7 @@ export COMPOSE_PROJECT_NAME
 
 WEB_IP_ADDRESS=$(docker inspect --format "{{.NetworkSettings.Networks.${COMPOSE_PROJECT_NAME}_default.IPAddress}}" ${COMPOSE_PROJECT_NAME}_web_1)
 docker-compose exec mysql mysql -u datavault -p -D datavault \
-    -e "DELETE FROM ArchiveStores WHERE storageClass = 'org.datavaultplatform.common.storage.impl.S3Cloud';" \
-    -e "UPDATE ArchiveStores SET storageClass = 'org.datavaultplatform.common.storage.impl.LocalFileSystem', label = 'Default archive store (Local)';" \
+    -e "DELETE FROM ArchiveStores WHERE storageClass != 'org.datavaultplatform.common.storage.impl.S3Cloud';" \
     -e "UPDATE Clients SET ipAddress = '${WEB_IP_ADDRESS}';" \
     -e "SELECT * FROM ArchiveStores;" \
 
