a4130738348f83715e5ae0733acd8cb4a3158d10
Steven Carlysle-Davies <steven.carlysle-davies@ed.ac.uk>
2018-05-07 14:40:02 +0100

    Create bucket for storing archived data

--- aws/s3.tf
+++ aws/s3.tf
@@ -11,3 +11,13 @@ resource "aws_s3_bucket" "state" {
     prevent_destroy = true
   }
 }
+
+resource "aws_s3_bucket" "archive" {
+  bucket = "rdss-datavault-archive"
+  acl    = "private"
+  tags = "${var.aws_cost_tags}"
+
+  versioning {
+    enabled = true
+  }
+}
